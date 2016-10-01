18280e5454dc3169e23c44085a8e5cdf8caa9611
olandahl <Work@Olofs-MacBook-Air.local>
2016-10-01 20:48:01 +0200

    Added navbar component

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
+
 @Component({
-  selector: 'my-app',
-  template: '<h1>Olof Landahl</h1>'
+  selector: 'app',
+  templateUrl: 'app/app.component.html'
 })
+
 export class AppComponent { }
