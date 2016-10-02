2a0fde9ac1f31529272cdb324e1f1b5463569619
oloflandahl <oloflandahl@hotmail.com>
2016-10-02 22:23:44 +0200

    Merged navbar with app header

--- app/app-header/navbar/navbar.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'navbar',
-  templateUrl: 'app/app-header/navbar/navbar.html',
-  styleUrls: ['app/app-header/navbar/navbar.css']
-})
-
-export class NavbarComponent { }
--- app/app.module.ts
+++ app/app.module.ts
@@ -2,11 +2,10 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent }   from './app.component';
 import { AppHeaderComponent }   from './app-header/app-header.component';
-import { NavbarComponent }   from './app-header/navbar/navbar.component';
 
 @NgModule({
   imports:      [ BrowserModule ],
-  declarations: [ AppComponent, AppHeaderComponent, NavbarComponent ],
+  declarations: [ AppComponent, AppHeaderComponent ],
   bootstrap:    [ AppComponent ]
 })
 
