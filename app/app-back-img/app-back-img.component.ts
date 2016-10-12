5c898c866a92ca154abccd60a4f3681a3762dc62
oloflandahl <oloflandahl@hotmail.com>
2016-10-12 22:55:39 +0200

    Adde background image component + minor fixes

---
+++ app/app-back-img/app-back-img.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-back-img',
+  templateUrl: 'app/app-back-img/app-back-img.html',
+  styleUrls: ['app/app-back-img/app-back-img.css']
+})
+
+export class AppBackImgComponent { }
--- app/app.module.ts
+++ app/app.module.ts
@@ -2,10 +2,11 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent }   from './app.component';
 import { AppHeaderComponent }   from './app-header/app-header.component';
+import { AppBackImgComponent }   from './app-back-img/app-back-img.component';
 
 @NgModule({
   imports:      [ BrowserModule ],
-  declarations: [ AppComponent, AppHeaderComponent ],
+  declarations: [ AppComponent, AppHeaderComponent, AppBackImgComponent ],
   bootstrap:    [ AppComponent ]
 })
 
