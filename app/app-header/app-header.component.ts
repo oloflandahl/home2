ce251fe256b3e725189dcfdc06ce32869d54d0a1
oloflandahl <oloflandahl@hotmail.com>
2016-11-05 20:32:11 +0100

    Sticky header

--- app/app-header/app-header.component.ts
+++ app/app-header/app-header.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, Inject, OnInit } from "@angular/core";
+import { DOCUMENT } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-header',
@@ -6,4 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['app/app-header/app-header.css']
 })
 
-export class AppHeaderComponent { }
+export class AppHeaderComponent implements OnInit {
+
+  public isSticky: boolean = false;
+
+  constructor(@Inject(DOCUMENT) private document: Document) { }
+
+  ngOnInit() { }
+
+  @HostListener("window:scroll", [])
+  onWindowScroll() {
+    this.isSticky = this.document.body.scrollTop > 0;
+  }
+}
