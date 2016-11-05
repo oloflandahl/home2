2ab9a9839c2504145e379df8c3cd0a1d8d28c7a1
oloflandahl <oloflandahl@hotmail.com>
2016-11-05 21:10:09 +0100

    Header slide down on init

--- app/app-header/app-header.component.ts
+++ app/app-header/app-header.component.ts
@@ -1,4 +1,4 @@
-import { Component, HostListener, Inject, OnInit } from "@angular/core";
+import { Component, HostListener, Inject } from "@angular/core";
 import { DOCUMENT } from '@angular/platform-browser';
 
 @Component({
@@ -9,11 +9,15 @@ import { DOCUMENT } from '@angular/platform-browser';
 
 export class AppHeaderComponent implements OnInit {
 
+  public isInit: boolean = false;
   public isSticky: boolean = false;
 
-  constructor(@Inject(DOCUMENT) private document: Document) { }
-
-  ngOnInit() { }
+  constructor(@Inject(DOCUMENT) private document: Document) {
+    var self = this;
+    setTimeout(function() {
+      self.isInit = true;
+    }, 0);
+  }
 
   @HostListener("window:scroll", [])
   onWindowScroll() {
