ba4f4dbb3b9c1d67698ab97d6c75870a13d11848
oloflandahl <oloflandahl@hotmail.com>
2017-06-27 16:29:19 +0200

    Minor fixes

--- src/app/app-cards/person-card/person-card-info-items.ts
+++ src/app/app-cards/person-card/person-card-info-items.ts
@@ -2,8 +2,8 @@ import dateService from '../../services/date-service';
 import {KeyValueItem} from '../key-value-item';
 
 export const INFO_ITEMS: KeyValueItem[] = [
-    {translationKey: 'PersonInfo_Age', value: dateService.getYearsToNow(1982, 8, 3).toFixed(0)},
-    {translationKey: 'PersonInfo_Years_Experience', value: dateService.getYearsToNow(2008, 2, 15).toFixed(0)},
+    {translationKey: 'PersonInfo_Age', value: String(dateService.getYearsToNow(1982, 8, 3))},
+    {translationKey: 'PersonInfo_Years_Experience', value: String(dateService.getYearsToNow(2008, 2, 15))},
     {translationKey: 'PersonInfo_City', value: 'Lund'},
     {translationKey: 'PersonInfo_Employment', value: 'HiQ Skåne'},
     {translationKey: 'PersonInfo_Email', value: 'oloflandahl@gmail.com'},
