e43a4494c0e6722408d77be09bd2185c2fe4928e
oloflandahl <olof.landahl@hiq.se>
2017-07-03 11:52:11 +0200

    Fixed scrolling to timeline

--- src/app/app-header/app-header-menu-items.ts
+++ src/app/app-header/app-header-menu-items.ts
@@ -3,5 +3,5 @@ import {MenuItem} from './menu-item';
 export const MENU_ITEMS: MenuItem[] = [
     {id: 'about', translationKey: 'About_Title', isActive: true},
     {id: 'skills', translationKey: 'Skills_Title', isActive: false},
-    {id: 'exp', translationKey: 'Experience_Title', isActive: false}
+    {id: 'experience', translationKey: 'Experience_Title', isActive: false}
 ];
