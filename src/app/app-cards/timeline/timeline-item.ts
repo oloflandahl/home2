d2541703483ec4b6942d5407a70797a40540389a
oloflandahl <olof.landahl@hiq.se>
2017-07-03 15:21:35 +0200

    Added icons

--- src/app/app-cards/timeline/timeline-item.ts
+++ src/app/app-cards/timeline/timeline-item.ts
@@ -1,8 +1,10 @@
 export interface TimelineItem {
   startYear: number;
   endYear: number;
+  logo: string;
   city: string;
   title: string;
   subTitleTranslationKey: string;
   descriptionTranslationKey: string;
+  url: string;
 }
--- src/app/app-cards/timeline/timeline-items.ts
+++ src/app/app-cards/timeline/timeline-items.ts
@@ -2,12 +2,12 @@ import {TimelineItem} from './timeline-item';
 
 // TODO Add icons, technologies
 export const TIMELINE_ITEMS: TimelineItem[] = [
-	{ startYear: 2016, endYear: 2017, city: "Malmö", title: 'HiQ - Securitas', subTitleTranslationKey: 'Role_WebDeveloper', descriptionTranslationKey: 'Experience_Securitas' },
-	{ startYear: 2015, endYear: 2016, city: "Malmö", title: 'HiQ - Tunstall', subTitleTranslationKey: 'Role_WebDeveloper', descriptionTranslationKey: 'Experience_Tunstall' },
-	{ startYear: 2016, endYear: 2017, city: "Malmö", title: 'HiQ', subTitleTranslationKey: 'Role_CodeTestVerifier', descriptionTranslationKey: 'Experience_HiQ' },
-	{ startYear: 2014, endYear: 2015, city: "Göteborg", title: 'HiQ - Telia', subTitleTranslationKey: 'Role_WebDeveloper', descriptionTranslationKey: 'Experience_Telia' },
-	{ startYear: 2013, endYear: 2014, city: "Sydney", title: 'DiUS - Vodafone', subTitleTranslationKey: 'Role_WebDeveloper', descriptionTranslationKey: 'Experience_Vodafone' },
-	{ startYear: 2008, endYear: 2013, city: "Göteborg", title: 'TIBCO Spotfire', subTitleTranslationKey: 'Role_SoftwareDeveloper', descriptionTranslationKey: 'Experience_Tibco' },
-	{ startYear: 2008, endYear: 2008, city: "Göteborg", title: 'Carmenta', subTitleTranslationKey: 'Role_SoftwareDeveloper', descriptionTranslationKey: 'Experience_Carmenta' },
-	{ startYear: 2002, endYear: 2008, city: "Norrköping", title: 'Linköpings Universitet', subTitleTranslationKey: 'Degree_Medieteknik', descriptionTranslationKey: 'Experience_Linkoping' }
+	{ startYear: 2016, endYear: 2017, logo: "securitas", city: "Malmö", title: 'HiQ - Securitas', subTitleTranslationKey: 'Role_WebDeveloper', descriptionTranslationKey: 'Experience_Securitas', url: 'http://www.securitas.se/' },
+	{ startYear: 2015, endYear: 2016, logo: "tunstall", city: "Malmö", title: 'HiQ - Tunstall', subTitleTranslationKey: 'Role_WebDeveloper', descriptionTranslationKey: 'Experience_Tunstall', url: 'http://www.tunstall.se/' },
+	{ startYear: 2016, endYear: 2017, logo: "hiq", city: "Malmö", title: 'HiQ', subTitleTranslationKey: 'Role_CodeTestVerifier', descriptionTranslationKey: 'Experience_HiQ', url: 'http://hiq.se/' },
+	{ startYear: 2014, endYear: 2015, logo: "telia", city: "Göteborg", title: 'HiQ - Telia', subTitleTranslationKey: 'Role_WebDeveloper', descriptionTranslationKey: 'Experience_Telia', url: 'https://www.telia.se/privat' },
+	{ startYear: 2013, endYear: 2014, logo: "vodafone", city: "Sydney", title: 'DiUS - Vodafone', subTitleTranslationKey: 'Role_WebDeveloper', descriptionTranslationKey: 'Experience_Vodafone', url: 'http://www.vodafone.com.au/' },
+	{ startYear: 2008, endYear: 2013, logo: "spotfire", city: "Göteborg", title: 'TIBCO Spotfire', subTitleTranslationKey: 'Role_SoftwareDeveloper', descriptionTranslationKey: 'Experience_Tibco', url: 'https://spotfire.tibco.com/' },
+	{ startYear: 2008, endYear: 2008, logo: "carmenta", city: "Göteborg", title: 'Carmenta', subTitleTranslationKey: 'Role_SoftwareDeveloper', descriptionTranslationKey: 'Experience_Carmenta', url: 'http://www.carmenta.com' },
+	{ startYear: 2002, endYear: 2008, logo: "liu", city: "Norrköping", title: 'Linköpings Universitet', subTitleTranslationKey: 'Degree_Medieteknik', descriptionTranslationKey: 'Experience_Linkoping', url: 'http://liu.se/' }
 ]
