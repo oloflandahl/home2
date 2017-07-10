10849ef30269065ca7990342a3d97069e90f1432
oloflandahl <oloflandahl@hotmail.com>
2017-07-11 01:00:39 +0200

    Improved interests card

--- src/app/data/interest-items.ts
+++ src/app/data/interest-items.ts
@@ -1,12 +1,11 @@
 import { IconItem } from '../models/icon-item';
 
+// Can only contain 6 items
 export const INTEREST_ITEMS: IconItem[] = [
   { iconName: 'airport', title: 'Interest_Travelling' },
   { iconName: 'restaurant', title: 'Interest_Food' },
   { iconName: 'bar', title: 'Interest_Beer' },
   { iconName: 'trail-walking', title: 'Interest_Nature' },
   { iconName: 'surfing', title: 'Interest_Surfing' },
-  { iconName: 'gym', title: 'Interest_Gym' },
-  { iconName: 'point-of-interest', title: 'Interest_Photo' },
-  { iconName: 'library', title: 'Interest_Reading' }
+  { iconName: 'point-of-interest', title: 'Interest_Photo' }
 ];
