aa251b0d52a805dc712a3e91473d750780e97a66
oloflandahl <olof.landahl@hiq.se>
2017-07-12 11:28:47 +0200

    Modified interests

--- src/app/data/interest-items.ts
+++ src/app/data/interest-items.ts
@@ -1,11 +1,12 @@
 import { IconItem } from '../models/icon-item';
 
-// Can only contain 6 items
+// Can only contain 7 items
 export const INTEREST_ITEMS: IconItem[] = [
-  { iconName: 'airport', title: 'Interest_Travelling' },
-  { iconName: 'trail-walking', title: 'Interest_Nature' },
-  { iconName: 'surfing', title: 'Interest_Surfing' },
-  { iconName: 'point-of-interest', title: 'Interest_Photo' },
-  { iconName: 'restaurant', title: 'Interest_Food' },
-  { iconName: 'bar', title: 'Interest_Beer' }
+  { iconPrefix: 'fa', iconName: 'music', title: 'Interest_Music' },
+  { iconPrefix: 'fa', iconName: 'futbol-o', title: 'Interest_Football' },
+  { iconPrefix: 'map-icon', iconName: 'trail-walking', title: 'Interest_Nature' },
+  { iconPrefix: 'map-icon', iconName: 'surfing', title: 'Interest_Surfing' },
+  { iconPrefix: 'fa', iconName: 'camera', title: 'Interest_Photo' },
+  { iconPrefix: 'map-icon', iconName: 'restaurant', title: 'Interest_Food' },
+  { iconPrefix: 'map-icon', iconName: 'bar', title: 'Interest_Beer' }
 ];
--- src/app/models/icon-item.ts
+++ src/app/models/icon-item.ts
@@ -1,4 +1,5 @@
 export interface IconItem {
   title: string;
+  iconPrefix?: string;
   iconName: string;
 }
