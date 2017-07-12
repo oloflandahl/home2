625751c44cf5f915c18467e23f2be6d54487d2a7
oloflandahl <olof.landahl@hiq.se>
2017-07-12 13:43:17 +0200

    Modified social items

--- src/app/data/social-link-items.ts
+++ src/app/data/social-link-items.ts
@@ -1,10 +1,10 @@
 import { SocialLinkItem } from '../models/social-link-item';
 
 export const SOCIAL_LINK_ITEMS: SocialLinkItem[] = [
-    {url: 'https://www.facebook.com/olof.landahl', iconName: 'facebook', title: 'Facebook'},
     {url: 'https://www.linkedin.com/in/olof-landahl-91782a37', iconName: 'linkedin', title: 'LinkedIn'},
     {url: 'https://github.com/oloflandahl', iconName: 'github', title: 'Github'},
     {url: 'https://plus.google.com/u/0/111518842534028383812', iconName: 'google-plus', title: 'Google+'},
     {url: 'https://securitashiq.slack.com/team/oloflandahl', iconName: 'slack', title: 'Slack'},
-    {url: 'https://www.last.fm/user/oloflandahl', iconName: 'lastfm', title: 'LastFM'}
+    {url: 'https://www.last.fm/user/oloflandahl', iconName: 'lastfm', title: 'LastFM'},
+    {url: 'mailto:oloflandahl@gmail.com', iconName: 'envelope-o', title: 'Email'}
 ];
