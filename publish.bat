742c645119e69164a445a004e9fa70dd3c9079a2
oloflandahl <olof.landahl@hiq.se>
2017-07-20 11:03:05 +0200

    Publish

--- publish.bat
+++ publish.bat
@@ -1,1 +1,5 @@
-git subtree push --prefix dist origin gh-pages
+start /wait ng build
+start /wait git add .
+start /wait git commit -a -m "Publish"
+start /wait git push
+start /wait git subtree push --prefix dist origin gh-pages
