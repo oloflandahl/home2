ce8a4f4e1df16e23f2da9837aab37f9e8af9d125
oloflandahl <olof.landahl@hiq.se>
2017-07-20 11:15:04 +0200

    Removed script files

--- publish.bat
+++
@@ -1,4 +0,0 @@
-start /wait git add .
-start /wait git commit -a -m "Publish"
-start /wait git push
-start /wait git subtree push --prefix dist origin gh-pages
--- publish.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-git add . && git commit -a -m "Publish" && git push && git subtree push --prefix dist origin gh-pages
