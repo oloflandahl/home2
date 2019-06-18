217a84b281adaa909ee8e16fac5ab1b7892b55a7
Olof Landahl <olof.landahl@hiq.se>
2019-06-18 16:39:52 +0200

    Fixed pbolaget logo issue. Updated personal info and skills

--- src/app/data/person-info-items.ts
+++ src/app/data/person-info-items.ts
@@ -4,7 +4,7 @@ import { KeyValueItem } from '../models/key-value-item';
 export const INFO_ITEMS: KeyValueItem[] = [
     {key: 'PersonInfo_Age', value: dateService.getYearsToNow(1982, 8, 3)},
     {key: 'PersonInfo_Years_Experience', value: dateService.getYearsToNow(2008, 2, 15)},
-    {key: 'PersonInfo_City', value: 'Lund'},
-    {key: 'PersonInfo_Employment', value: 'HiQ Skåne'},
+    {key: 'PersonInfo_City', value: 'Göteborg'},
+    {key: 'PersonInfo_Employment', value: 'HiQ Göteborg'},
     {key: 'PersonInfo_Email', value: 'oloflandahl@gmail.com'}
 ];
--- src/app/data/skill-items.ts
+++ src/app/data/skill-items.ts
@@ -2,9 +2,10 @@ import { KeyValueItem } from '../models/key-value-item';
 
 export const SKILL_ITEMS: KeyValueItem[] = [
     {key: 'JavaScript', value: 90},
+    {key: 'TypeScript', value: 80},
     {key: 'HTML/CSS', value: 90},
     {key: 'AngularJS', value: 90},
-    {key: 'Angular', value: 40},
+    {key: 'Angular', value: 90},
     {key: 'KnockoutJS', value: 95},
     {key: 'ReactJS', value: 50},
     {key: 'Bootstrap', value: 90},
