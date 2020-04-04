14ca025b5a4e35367bc9c2a356c2e4ed7f6a9a92
Olof Landahl <oloflandahl@gmail.com>
2020-04-04 11:30:32 +0200

    Fixed share and build issues

--- src/app/data/social-link-items.ts
+++ src/app/data/social-link-items.ts
@@ -3,16 +3,14 @@ import { IconLinkItem } from '../models/icon-link-item';
 const SOCIAL_LINK_ITEMS: IconLinkItem[] = [
     { url: 'https://www.linkedin.com/in/oloflandahl', iconName: 'linkedin', title: 'LinkedIn' },
     { url: 'https://github.com/oloflandahl', iconName: 'github', title: 'Github' },
-    { url: 'https://securitashiq.slack.com/team/oloflandahl', iconName: 'slack', title: 'Slack' },
     { url: 'https://www.last.fm/user/oloflandahl', iconName: 'lastfm', title: 'LastFM' },
     { url: 'mailto:oloflandahl@gmail.com', iconName: 'envelope-o', title: 'Email', openOnPage: true }
 ];
 
-const share = (navigator as any).share;
-if (share) {
+if ((navigator as any).share) {
     const onShare = (event: MouseEvent) => {
         event.preventDefault();
-        share({
+        (navigator as any).share({
             title: 'Olof Landahl',
             url: 'https://oloflandahl.com/'
         });
